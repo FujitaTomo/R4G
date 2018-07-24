fd18579b66e1ae53f35cebe42c01e4f039f24902
FujitaTomo <ponrere@gmail.com>
2018-07-24 10:08:45 +0000

    管理者メニュー調整

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,6 +2,8 @@ class AdminsController < ApplicationController
   def top
   	@search = Item.ransack(params[:q])
     @items = @search.result
+    @usearch = User.ransack(params[:q])
+    @users = @usearch.result
   end
 
   private
