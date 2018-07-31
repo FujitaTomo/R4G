66d88306bed130a564e47f97bd1486e044d923e0
FujitaTomo <ponrere@gmail.com>
2018-07-31 10:44:25 +0000

    top仮完成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -5,5 +5,8 @@ class TopsController < ApplicationController
   	@items = Item.order(created_at: :desc).limit(5)
     @votes = Vote.order(created_at: :desc).limit(5)
     @boards = Board.order(created_at: :desc).limit(5)
+    @itemranks =Item.where('sale_date >= ?', Date.today.beginning_of_month)
+    @item_ids = @itemranks.pluck(:id)
+    @ranks = @itemranks.find(Vote.where(item_id: @item_ids).group(:item_id).order("count(item_id) desc").limit(3).pluck(:item_id))
   end
 end
